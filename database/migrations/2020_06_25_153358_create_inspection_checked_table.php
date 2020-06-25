40abf7ae145004553be071deae0858c4f8c55c1b
wi11z <9alile1@gmail.com>
2020-06-25 19:02:18 +0300

    Inspection database

---
+++ database/migrations/2020_06_25_153358_create_inspection_checked_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInspectionCheckedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inspection_checked', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('checklist_id')->constrained()->onDelete('cascade');
+            $table->mediumText('action_required')->nullable();
+            $table->boolean('status');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inspection_checked');
+    }
+}
