95a567a00ae3ce95234225dfce0648418b3eb871
wi11z <9alile1@gmail.com>
2020-09-14 16:15:10 +0300

    Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\User;
+use App\Accident;
+use App\InspectionChecked;
+use App\RiskIdentification;
+use App\Task;
+use App\Training;
 
 class HomeController extends Controller
 {
@@ -23,6 +30,26 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        // $users = DB::table('users')->where('')
+        $users = User::where('is_admin', false)->get();
+        $accidents = DB::table('accidents')->get();
+        $inspections = DB::table('inspection_checked')->get();
+        $tasks = DB::table('tasks')->get();
+        $events = DB::table('trainings')->get();
+
+        
+        // Tasks Percentages
+        $tasks_percent = (int) ($tasks->where('completed', TRUE)->count()/$tasks->count() * 100);
+
+        //  return dd($tasks_percent);
+
+        return view('home', [
+            'users' => $users,
+            'accidents' => $accidents,
+            'inspections' => $inspections,
+            'tasks_percent' => $tasks_percent,
+            'events' => $events,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,86 @@
 @extends('dashboard.index')
 
 @section('content')
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- Content Row -->
+    <div class="row">
 
-                    You are logged in!
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Users</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $users->count() }}</div>
                 </div>
+                <div class="col-auto">
+                  <i class="fas fa-users fa-2x text-gray-300"></i>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</div> --}}
-    <!-- Page Heading -->
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Welcome {{ Auth::user()->name}}</h1>
-    {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
-</div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Reported Accidents</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $accidents->count() }}</div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-hospital-alt fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
+                  <div class="row no-gutters align-items-center">
+                    <div class="col-auto">
+                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $tasks_percent }}%</div>
+                    </div>
+                    <div class="col">
+                      <div class="progress progress-sm mr-2">
+                        <div class="progress-bar bg-info" role="progressbar" style="width: {{ $tasks_percent }}%" aria-valuenow="{{ $tasks_percent }}" aria-valuemin="0" aria-valuemax="100"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Pending Requests Card Example -->
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Training Conducted</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">{{  $events->count() }}</div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-chalkboard-teacher fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
 @endsection
